e1b8b631d1dfb96e919a05ab630684e9033cd4bf
Eden <eden@nurigo.net>
2018-02-10 02:37:44 +0900

    행운아이템 시뮬레이터 골격

---
+++ index.js
@@ -0,0 +1,149 @@
+var itemSet = {
+  bitem: {
+    info: {
+
+    },
+    items: [
+      ["샐러리맨의 월급 ", "낡은 보물지도", "블랙카드", "고장난 저울", "유리 구슬", "위조된 땅 문서", "전설의 방망이", "VIP항공 티켓", "코끼리 목각 인형", "악마의 눈", "독수리 발톱", "모험왕의 기념주화", "힐링 여행권", "도신의 장갑", "사장님의 힐링 여행권", "뜻밖의 초대권", "보안관 배지", "탑레이서의 헬멧", "대가의 건설장비", "블랙홀 발생기", "사장님의 긴급 호출", "풍요신의 문장", "황금 프리패스", "여행의 동반자", "나나의 곰돌이"]
+    ]
+  },
+  aitem: {
+    items: [
+      ["오래된 오쿨렐레", "라차드우의 트럼프 카드", "아라의 초크", "아리안의 손거울", "신호탄", "운석조각", "타란튤라의 레이저건", "믿을만한 신용증서", "금이 간 레코드판", "야바위꾼의 컵 세트", "루피의 쿠키", "에디가 만든 낚싯대", "패티의 장갑", "포비의 카메라", "전설의 방망이", "VIP항공 티켓", "코끼리 목각 인형", "악마의 눈", "독수리 발톱", "낡은 보물지도", "매혹의 향수", "블랙카드", "힐링 여행권", "도신의 장갑", "모험왕의 기념주화", "고장난 네비게이션", "나나의 곰돌이", "사장님의 힐링 여행권", "뜻밖의 초대권", "보안관 배지", "탑레이서의 헬멧", "대가의 건설장비", "사장님의 긴급 호출", "풍요신의 문장", "황금 프리패스", "여행의 동반자", "블랙홀 발생기"]
+    ]
+  },
+  apitem: {
+    info: {
+    },
+    items: [
+      ["오래된 오쿨렐레", "라차드우의 트럼프 카드", "아라의 초크", "아리안의 손거울", "신호탄", "운석조각", "타란튤라의 레이저건", "믿을만한 신용증서", "금이 간 레코드판", "네잎 클로버", "검은 장갑", "루피의 쿠키", "에디가 만든 낚싯대", "패티의 장갑", "포비의 카메라", "전설의 방망이", "VIP항공 티켓", "코끼리 목각 인형", "악마의 눈", "독수리 발톱", "낡은 보물지도", "매혹의 향수", "블랙카드", "힐링 여행권", "도신의 장갑", "왕가의 팬던트", "맹독 화살촉", "모험왕의 기념주화", "고장난 네비게이션", "나나의 곰돌이", "야바위꾼의 컵 세트", "왕가의 기념비", "뜻밖의 초대권", "보안관 배지", "탑레이서의 헬멧", "대가의 건설장비", "블랙홀 발생기", "사장님의 긴급 호출", "풍요신의 문장", "황금 프리패스", "여행의 동반자", "잘못된 가이드북", "불카토스의 망치", "재벌가의 상속문서", "마그네틱코어", "대지주의 깃발", "약속된 힐링 여행권"]
+    ]
+  },
+  sitem: {
+    info: {
+      percentageInfo: {
+        abs: 100,
+        percentages: [
+          0.07,
+          0.05,
+        ]
+      }
+    },
+    items: [
+      ["독수리 발톱", "낡은 보물지도", "멈추지 않는 팽이", "고장난 네비게이션", "여행자의 나침반", "매혹의 향수", "황금 복돼지", "힐링 여행권", "도신의 장갑", "왕가의 펜던트", "4차원의 유리 구슬", "마이더스의 지도", "마트료시카", "맹독 화살촉", "블랙카드", "모험왕의 기념 주화", "튼튼한 편자", "구원의 골든벨", "보안관 배지", "물귀신의 소복", "궁극의 합의서", "전파 증폭기", "휴대용 구급팩", "현상금 전단지", "왕가의 기념비", "축제용 폭죽", "전장의 황금 스패너", "전장의 만능 연장세트", "야바위꾼의 컵 세트", "풍요신의 문장", "뜻밖의 초대권", "사장님의 긴급 호출", "제사장의 주술봉", "볼카토스의 망치", "프로즌 해머", "명장의 지휘봉", "블랙홀 발생기", "여행의 동반자", "승리의 확성기", "공항 택시", "사장님의 힐링 여행권", "황금 프리패스", "위조된 설계 도면", "재벌가의 상속문서", "대지주의 깃발", "작은 고양이 장식", "뽀로로의 고글", "크롱의 장난감 자동차", "투우사의 검", "기묘한 주사기", "토파즈 지팡이", "탑레이서의 헬멧", "나나의 곰돌이", "고귀한 왕실 반지", "빅토리아의 부채", "모험가의 채찍", "커스텀 글라스", "행운의 공작 깃털", "오래된 램프", "샐러브리티의 목도리", "용도 변경된 제트팩", "저명작가의 필름", "장인이 만든 통기타", "디바의 마이크", "대가의 건설장비", "잘못된 가이드북", "강화된 마그네틱 코어", "명인의 건설비책", "겜블러의 마블 트럼프", "겜블러의 건설 트럼프", "겜블러의 트릭"],
+      ["스피드 레이서의 헬멧", "추격자의 헬멧", "초인의 건설장비", "풍선 별장", "강화된 블랙홀 발생기", "어디서나 힐링 여행권", "행운의 트램폴린"],
+      ["공포의 빨간딱지", "그림자 인술서", "비눗방울 장난감", "블랙홀 추진기", "헤르메스의 헬멧", "블랙홀 추진기", "헤르메스의 헬멧", "제왕의 건설장비", "각성 도깨비 곰돌이", "원격 마그네틱 코어", "뉴타운 확장권", "곡예사의 트램폴린", "전자기장 발생기", "괴도의 인술서", "지옥행 가이드북", "순간이동 초대권", "강화된 블랙홀 추진기", "파라오의 헬멧", "스파이의 설계도면", "공포의 건설장비", "반중력 트램폴린", "블랙홀 증폭기", "일단정지 경찰차", "긴급탈출 제트팩", "파워 펀치건", "최첨단 건설장비", "천사의 비눗방울 장난감", "정복자의 깃발", "거장의 건설비책", "황제의 건설장비", "비눗방울 소환 부적", "각성 사신 곰돌이", "슈퍼 강화된 마그네틱 코어", "빛나는 유혹의 반지", "블랙홀 주술 지팡이", "권력자의 배지", "환상의 팔레트", "소형 블랙홀 증폭기", "로켓 펀치건", "원격 건설 드론", "전율의 빨간딱지", "드래곤 헬멧", "점핑 슈즈", "듀얼 마그네틱 코어"],
+    ]
+  },
+  eventItem: {
+    info:  {
+    },
+    items: [
+      ['이벤트아이템']
+    ]
+  }
+}
+function getItemLevel () {
+  var itemLevelSet = {
+    eventItem: 8,
+    bitem: 72, // => 0~71
+    aitem: 17, // => 72~88
+    apitem: 1, // => 90~98
+    sitem: 2, // => 99~100
+  }
+  var abs = 100
+  var itemran = Math.floor(Math.random() * abs)
+  var selItem
+  Object.keys(itemLevelSet).every((key, index) => {
+    abs = abs - itemLevelSet[key]
+    if (itemran >= abs) {
+      selItem = key
+      return false
+    }
+    return true
+  })
+  return selItem
+}
+function getItemProduct () {
+  var itemLevel = getItemLevel()
+  var item = itemSet[itemLevel]
+  var itemList = item.items[0]
+  var keyNumber = Math.floor(Math.random() * itemList.length)
+  if (item.info && item.info.percentageInfo) {
+    var pInfo = item.info.percentageInfo
+    var percentages = pInfo.percentages
+    var abs = pInfo.abs
+    var prodran = Math.floor(Math.random() * (pInfo.abs * 100)) / 100
+    percentages.every((percent, index) => {
+      matchran = item.items[index + 1].length * percent
+      abs = abs - matchran
+      if (prodran >= abs) {
+        console.log('대박!')
+        itemList = item.items[index + 1]
+        keyNumber = Math.floor(Math.random() * itemList.length)
+        return false
+      }
+      return true
+    })
+  }
+  return { name: itemList[keyNumber], level: itemLevel }
+}
+var diamond = 0
+var data = {
+  bitem: {
+    count: 0,
+    data: []
+  },
+  aitem: {
+    count: 0,
+    data: []
+  },
+  apitem: {
+    count: 0,
+    data: []
+  },
+  sitem: {
+    count: 0,
+    data: []
+  },
+  eventItem: {
+    count: 0,
+    data: []
+  },
+}
+
+var interval
+function run() {
+  interval = setInterval(() => {
+    diamond += 100
+    for (var i = 0; i < 5; i++) {
+      var product = getItemProduct()
+      var color
+      if (product.level === 'sitem') {
+        color = '#efb336'
+        data[product.level].count = data[product.level].count += 1
+        data[product.level].data.push(product.name)
+      } else if (product.level === 'apitem') {
+        color = '#77517e'
+        data[product.level].count = data[product.level].count += 1
+        data[product.level].data.push(product.name)
+      } else if (product.level === 'aitem') {
+        color = '#3284b4'
+        data[product.level].count = data[product.level].count += 1
+        data[product.level].data.push(product.name)
+      } else if (product.level === 'eventItem') {
+        color = '#f00'
+        data[product.level].count = data[product.level].count += 1
+        data[product.level].data.push(product.name)
+      } else {
+        color = '#b83e2f'
+        data[product.level].count = data[product.level].count += 1
+        data[product.level].data.push(product.name)
+      }
+      console.log('%c' + product.name + product.level + "\t" + '다이아소모 :' + diamond, 'color: ' + color)
+    }
+  }, 0)
+}
+function stop() {
+  clearInterval(interval)
+}
